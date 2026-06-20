e295c305d60491b1ae44eacf831f88a017817b6e
Sergey Chukhno <sergeychukhno@macbook-air-de-sergey.home>
2026-06-20 08:18:43 +0200

    fix(shim): fail fast when DYLD_INSERT_LIBRARIES is not set

--- client/src/shim.cpp
+++ client/src/shim.cpp
@@ -5,13 +5,15 @@
 
 int main() {
     const char* dylib_path = ::getenv("DYLD_INSERT_LIBRARIES");
-    if (dylib_path) {
-        void* handle = ::dlopen(dylib_path, RTLD_NOW | RTLD_GLOBAL);
-        if (!handle) {
-            ::fprintf(stderr, "[Shim] dlopen(%s) failed: %s\n",
-                      dylib_path, ::dlerror());
-            return 1;
-        }
+    if (!dylib_path || *dylib_path == '\0') {
+        ::fprintf(stderr, "[Shim] DYLD_INSERT_LIBRARIES is not set\n");
+        return 1;
+    }
+    void* handle = ::dlopen(dylib_path, RTLD_NOW | RTLD_GLOBAL);
+    if (!handle) {
+        ::fprintf(stderr, "[Shim] dlopen(%s) failed: %s\n",
+                  dylib_path, ::dlerror());
+        return 1;
     }
 
     ::pause();
